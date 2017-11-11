29d8bec16ffda1eecb2de181b151baea5c42c6bb
mthompsonc <m.paz.thompson@gmail.com>
2017-11-11 17:41:11 -0300

    Versión 0.0.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-document.getElementById('form').style.display='none';
+document.getElementById('form').style.display='none'; //escondiendo form
 
-var btn1 = document.getElementById('anadir');
+var btn1 = document.getElementById('anadir'); //
 
 console.log("holi1");
 
@@ -27,9 +27,24 @@ btn2.addEventListener('click', function(){
   var divbtn3 = document.createElement('div');
   var abtn3 = document.createElement('button');
   var tbtn3 = document.createTextNode('Añadir Tarea');
+  divbtn3.classList.add('btn3');
   abtn3.appendChild(tbtn3);
   divbtn3.appendChild(abtn3);
   container.appendChild(divbtn3);
 
+
+  abtn3.addEventListener('click', function(){
+    divbtn3.style.display='none';
+    var divtextarea1= document.createElement('div');
+    var textarea1 = document.createElement('textarea');
+    var btn4 = document.createElement('button');
+    var tbtn4= document.createTextNode('Añadir una tarea')
+    textarea1.classList.add('tarea1');
+    btn4.classList.add('btn4');
+    btn4.appendChild(tbtn4);
+    divtextarea1.appendChild(textarea1);
+    divtextarea1.appendChild(btn4);
+    container.appendChild(divtextarea1);
+ })
 }
 )
