f4850cb8079948d828c25bdf41f48f64278186cc
mthompsonc <m.paz.thompson@gmail.com>
2017-11-12 23:20:27 -0300

    Poniéndose Bellako

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,46 @@
-document.getElementById('form').style.display='none';
-var section =document.getElementById('cards'); //escondiendo form
+document.getElementById('form').style.display='none'; //escondiendo form
+var section =document.getElementById('cards'); //agregando en una variable el elemento con id cards (es la sección donde irán las tarjetas de trello)
 
-var btn1 = document.getElementById('anadir'); //
-
-console.log("holi1");
+var btn1 = document.getElementById('anadir'); //agregando en una variable el elemento con id anadir que es el primer boton para añadir una tarjeta
 
 btn1.addEventListener('click', function(){
 var container = document.getElementById('card');
 btn1.style.display = "none";
 document.getElementById('form').style.display='inline-block';
-});
+}); // en esta función se le da actividad al boton para añadir una tarjeta y para que aparezca nuestra form que da la opción de añadir un título.
 
-var btn2 = document.getElementById('SaveButton');
-console.log("holi2");
+var btn2 = document.getElementById('saveButton');
 btn2.addEventListener('click', function(){
   document.getElementById('form').style.display='none';
   var container = document.getElementById('contenedor');
   document.getElementById('card').style.float='left';
   document.getElementById('contenedor').style.float='left';
 
-  var ingreso = document.getElementById('primerInput').value;
+  var ingreso = document.getElementsByTagName('input')[1].value;
   var tText = document.createTextNode(ingreso);
-  var titulo = document.createElement('p');
+  var titulo = document.createElement('h4');
   var divTask = document.createElement('div');
 
   divTask.style.float='left';
-
-console.log("holi3");
+  ingreso.value='';
   divTask.classList.add('tTarjeta');
   titulo.appendChild(tText);
   divTask.appendChild(titulo);
   container.appendChild(divTask);
 
   section.insertBefore(contenedor, card);
+    //Función de click a partir del botón que está bajo del input para el título de la tarjeta. Se agrega el título en un nuevo div que irán dentro del div Contenedor.
+
   document.getElementById('form').style.display='inline-block';
-console.log("holi4");
+
+//
   var divbtn3 = document.createElement('div');
   var abtn3 = document.createElement('button');
   var tbtn3 = document.createTextNode('Añadir Tarea');
-  divbtn3.classList.add('btn3');
+  abtn3.classList.add('btn3');
   abtn3.appendChild(tbtn3);
   divbtn3.appendChild(abtn3);
   divTask.appendChild(divbtn3);
-
-
 
   abtn3.addEventListener('click', function(){
     divbtn3.style.display='none';
@@ -67,14 +64,13 @@ console.log("holi4");
       var lapiz = document.createElement('i');
       lapiz.classList.add('fa', 'fa-pencil');
       var divtarea = document.createElement('div');
+      divtarea.classList.add('divtarea');
       ptarea.appendChild(tareatext);
       divtarea.appendChild(ptarea);
       divtarea.appendChild(lapiz);
       divTask.insertBefore(divtarea, divtextarea1);
       ptarea.style.display='inline-block';
       lapiz.style.display='inline-block';
-      ptarea.remove.style;
-      lapiz.remove.style;
     })
  })
 }
