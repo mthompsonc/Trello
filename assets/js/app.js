02f9abe99f1b0cc400902b198b8f3c8b25be43b7
María Paz Thompson <m.paz.thompson@gmail.com>
2018-03-07 08:45:43 -0300

    agregando cambios en la funcionalidad y aplicando eslint

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,47 +13,46 @@ btn1.addEventListener('click', function() {
 
 var btn2 = document.getElementById('saveButton');
 btn2.addEventListener('click', function() {
-  document.getElementById('form').style.display = 'none';
+  // document.getElementById('form').style.display = 'none';
   var container = document.getElementById('contenedor');
   document.getElementById('card').style.float = 'left';
   document.getElementById('contenedor').style.float = 'left';
 
   var ingreso = document.getElementById('primerInput').value;
+  document.getElementById('primerInput').value = '';
   if (ingreso === null || ingreso === '') {
-    return alert('Debes ingresar un dato');
+    return alert('Debes ingresar un nombre para la lista!');
   } else {
     var tText = document.createTextNode(ingreso);
     var titulo = document.createElement('h4');
     var divTask = document.createElement('div');
 
     divTask.style.float = 'left';
-    ingreso.value = '';
+    ingreso = '';
     divTask.classList.add('tTarjeta');
     titulo.appendChild(tText);
     divTask.appendChild(titulo);
     container.appendChild(divTask);
 
     section.insertBefore(contenedor, card);
-  // Función de click a partir del botón que está bajo del input para el título de la tarjeta. Se agrega el título en un nuevo div que irán dentro del div Contenedor.
+    // Función de click a partir del botón que está bajo del input para el título de la tarjeta. Se agrega el título en un nuevo div que irán dentro del div Contenedor.
+    document.getElementById('form').style.display = 'inline-block';
+
+    var divbtn3 = document.createElement('div');
+    var abtn3 = document.createElement('button');
+    var tbtn3 = document.createTextNode('Añadir Tarea');
+    abtn3.classList.add('btn3');
+
+    abtn3.appendChild(tbtn3);
+    divbtn3.appendChild(abtn3);
+    divTask.appendChild(divbtn3);
   }
-
-  document.getElementById('form').style.display = 'inline-block';
-
-  //
-  var divbtn3 = document.createElement('div');
-  var abtn3 = document.createElement('button');
-  var tbtn3 = document.createTextNode('Añadir Tarea');
-
-  abtn3.classList.add('btn3');
-
-  abtn3.appendChild(tbtn3);
-  divbtn3.appendChild(abtn3);
-  divTask.appendChild(divbtn3);
 
   abtn3.addEventListener('click', function() {
     divbtn3.style.display = 'none';
     var divtextarea1 = document.createElement('div');
     var textarea1 = document.createElement('textarea');
+    textarea1.placeholder = 'Ingresa tu nueva tarea.';
     var btn4 = document.createElement('button');
     var tbtn4 = document.createTextNode('Añadir una tarea');
 
@@ -69,18 +68,22 @@ btn2.addEventListener('click', function() {
     btn4.addEventListener('click', function() {
       var tareatextarea = textarea1.value;
       textarea1.value = '';
-      var tareatext = document.createTextNode(tareatextarea);
-      var ptarea = document.createElement('p');
-      var lapiz = document.createElement('i');
-      lapiz.classList.add('fa', 'fa-pencil');
-      var divtarea = document.createElement('div');
-      divtarea.classList.add('divtarea');
-      ptarea.appendChild(tareatext);
-      divtarea.appendChild(ptarea);
-      divtarea.appendChild(lapiz);
-      divTask.insertBefore(divtarea, divtextarea1);
-      ptarea.style.display = 'inline-block';
-      lapiz.style.display = 'inline-block';
+      if (tareatextarea === null || tareatextarea === '') {
+        return alert('Debes ingresar una tarea!');
+      } else {
+        var tareatext = document.createTextNode(tareatextarea);
+        var ptarea = document.createElement('p');
+        var lapiz = document.createElement('i');
+        lapiz.classList.add('fa', 'fa-pencil');
+        var divtarea = document.createElement('div');
+        divtarea.classList.add('divtarea');
+        ptarea.appendChild(tareatext);
+        divtarea.appendChild(ptarea);
+        divtarea.appendChild(lapiz);
+        divTask.insertBefore(divtarea, divtextarea1);
+        ptarea.style.display = 'inline-block';
+        lapiz.style.display = 'inline-block';
+      }
     });
   });
 });
